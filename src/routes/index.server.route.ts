ccdfe7178aac90e86ea76e81a862c0df8d2049c7
Bartłomiej Zimny <b.zimny@getprintbox.com>
2019-04-17 21:16:02 +0200

    fix

--- src/routes/index.server.route.ts
+++ src/routes/index.server.route.ts
@@ -4,6 +4,7 @@ import { emailController } from "../controllers/email.server.controller";
 
 export default class IndexRoute {
   constructor(app: Express) {
+    app.route("/").get(indexController.index);
     app.route("/contact").post(emailController.contact)
     app.route("/valuation").post(emailController.valuation)
   }
