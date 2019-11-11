0022931af0c741d77411682a36977e1e5b059fb0
Bartłomiej Zimny <bartlomiejzimny@outlook.com>
2019-11-11 20:21:20 +0100

    docker socket

--- src/config/express.ts
+++ src/config/express.ts
@@ -4,20 +4,9 @@ import * as cookieParser from 'cookie-parser';
 import * as express from 'express';
 import * as logger from 'morgan';
 import * as path from 'path';
-import * as mongoose from 'mongoose';
 
 export default function () {
     var app: express.Express = express();
-
-    for (let model of config.globFiles(config.models)) {
-        require(path.resolve(model));
-    }
-
-    if (config.useMongo) {
-        mongoose.connect(config.mongodb, {
-            promiseLibrary: global.Promise
-        }).catch(() => { console.log('Error connecting to mongos'); });
-    }
 
     app.set('views', path.join(__dirname, '../../src/views'));
     app.set('view engine', 'pug');
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import * as http from 'http';
 import config from './config/config';
-import { Socket } from './socket/index';
 
 const app = require('./config/express').default();
 
 const server: http.Server = new http.Server(app);
 
-new Socket(server);
 
 server.listen(config.port);
 
@@ -15,9 +13,5 @@ server.on('error', (e: Error) => {
 });
 
 server.on('listening', () => {
-  if (config.useMongo) {
-    console.log(`Server started on port ${config.port} on env ${process.env.NODE_ENV || 'dev'} dbcon ${config.mongodb}`);
-  } else {
     console.log(`Server started on port ${config.port} on env ${process.env.NODE_ENV || 'dev'}`);
-  }
 });
